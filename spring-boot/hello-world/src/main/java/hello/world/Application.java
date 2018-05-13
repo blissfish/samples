b5ab1fda8002bd774be97b499bc66759363967d2
markevertz <mark.evertz@gmx.de>
2018-05-13 19:33:29 +0200

    Added greetings

--- spring-boot/hello-world/src/main/java/hello/world/Application.java
+++ spring-boot/hello-world/src/main/java/hello/world/Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +23,12 @@ public class Application {
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+    
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
     @Bean

73608c8cfa86ca1c8477ab3ee1f484c0d3353be8
Mark Evertz <mark.evertz@gmx.de>
2018-05-26 17:39:01 +0200

    Changed random data creation to DataFactory impl.

--- spring-boot/web-socket-sample/src/main/java/web/socket/sample/RandomDataGeneratorService.java
+++ spring-boot/web-socket-sample/src/main/java/web/socket/sample/RandomDataGeneratorService.java
@@ -2,38 +2,36 @@ package web.socket.sample;
 
 import java.util.Random;
 
+import org.fluttercode.datafactory.NameDataValues;
+import org.fluttercode.datafactory.impl.DataFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.core.MessageSendingOperations;
-import org.springframework.messaging.simp.broker.BrokerAvailabilityEvent;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
-public class RandomDataGenerator implements
-    ApplicationListener<BrokerAvailabilityEvent> {
+@Service
+public class RandomDataGeneratorService {
 
-	private static final Logger log = LoggerFactory.getLogger(RandomDataGenerator.class);
+	private static final Logger log = LoggerFactory.getLogger(RandomDataGeneratorService.class);
+
+	private final DataFactory df;
     private final MessageSendingOperations<String> messagingTemplate;
 
     @Autowired
-    public RandomDataGenerator(
+    public RandomDataGeneratorService(
         final MessageSendingOperations<String> messagingTemplate) {
         this.messagingTemplate = messagingTemplate;
-    }
-
-    @Override
-    public void onApplicationEvent(final BrokerAvailabilityEvent event) {
-    }
+        this.df = new DataFactory();
+    }  
 
     @Scheduled(fixedDelay = 100)
     public void sendDataUpdates() {
-    	Integer random = new Random().nextInt(100);
+    	Integer random = df.getNumberBetween(0, 100);
     	log.info("Created Random "+random);
         this.messagingTemplate.convertAndSend(
-            "/topic/numbers", new Random().nextInt(100));
+            "/topic/numbers", random);
 
     }
     
